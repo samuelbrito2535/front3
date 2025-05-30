87cf911fad102bd81f280fe75452d8453116c401
samuelbrito2535 <samuelbrito2535@gmail.com>
2025-05-29 21:25:58 -0300

    ososo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const updateProductPrice = document.querySelector('#update-price');
 
 // Function to fetch all products from the server
 async function fetchProducts() {
-  const response = await fetch('http://3.135.191.232:3000/products');
+  const response = await fetch('http://3.149.10.77:3000/products');
   const products = await response.json();
 
   // Clear product list
@@ -54,7 +54,7 @@ addProductForm.addEventListener('submit', async event => {
 
 // Function to add a new product
 async function addProduct(name, price) {
-  const response = await fetch('http://3.135.191.232:3000/products', {
+  const response = await fetch('http://3.149.10.77:3000/products', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -66,7 +66,7 @@ async function addProduct(name, price) {
 
 // Function to delete a new product
 async function deleteProduct(id) {
-  const response = await fetch('http://3.135.191.232:3000/products' + id, {
+  const response = await fetch('http://3.149.10.77:3000/products/' + id, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
